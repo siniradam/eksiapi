ef45f36ec65b6ff1a75eeefd69b36f1a3f8aee10
Koray K <3178435+siniradam@users.noreply.github.com>
2023-02-27 14:59:10 -0800

    Minor changes (headers)

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import router from "./router.js";
+import { setHeaders } from "./src/utils/response.js";
 const app = express();
 //CORS, helmet, rate limit...
 
+app.use(setHeaders);
 app.use("/v1", router);
+
+app.get("/", (req, res, next) => {
+  res.json({ hello: Math.round(Date.now() / 1000) });
+});
 
 const port = process.env.PORT || 3000;
 // Start the server
--- src/utils/response.js
+++ src/utils/response.js
@@ -1,3 +1,16 @@
+export function setHeaders(req, res, next) {
+  res.setHeader("Powered", "Yep!");
+  res.setHeader("X-nananana", "Batman!");
+  res.setHeader("X-Powered-By", "OhShift");
+  res.setHeader(
+    "X-hacker",
+    "This is a proof of concept, please don't push it."
+  );
+  res.setHeader("X-GitHub-Repo", "https://github.com/siniradam/eksiapi");
+
+  next();
+}
+
 export async function response(data, status) {
   return {
     status: !status || status == 200,
